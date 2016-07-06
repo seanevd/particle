66adb9f7d4037c51dda68377d42b7792a983a916
sean <sean@nutrislice.com>
2016-07-06 12:34:16 -0600

    updated package

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
 app.use(function(req, res) {
   res.sendFile(__dirname + "/dist/index.html");
 });
