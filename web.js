1c10c2d47d354535f4390c333e64a403a8967721
sean <sean@nutrislice.com>
2016-07-06 13:03:06 -0600

    updated server

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(express.logger('dev'));
+// app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 3000);
 app.use(function(req, res) {
