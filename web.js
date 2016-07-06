a390097fd0b817e64d0a6cb2e5eb18099a85787f
sean <sean@nutrislice.com>
2016-07-06 12:22:17 -0600

    update to server

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
+var express = require('express');
+var app = express();
 
-  app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
+app.use(function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
