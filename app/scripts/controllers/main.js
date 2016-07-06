0ad3b712eb250ce5da873ee237f8898eb9eac899
sean <sean@nutrislice.com>
2016-07-06 17:01:20 -0600

    updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,16 +8,20 @@
  * Controller of the particleApp
  */
 angular.module('particleApp')
-  .controller('MainCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+  .controller('MainCtrl', ['$scope', '$http', '$rootScope', 'moment', function($scope, $http, $rootScope, Moment) {
+
     var at = 'd6576383889e1526c95853391923584b508071c4';
     var url = 'https://api.particle.io/v1/devices?access_token=' + at;
     var _ = $rootScope._;
+    $scope.device = {};
 
-    $http.get(url).then(function(response) {
-      $scope.devices = response.data;
-      $scope.currentDevice = currentDevice(response.data[0].id);
-      fetchData($scope.currentDevice.id);
-    });
+    $scope.init = function() {
+      $http.get(url).then(function(response) {
+        $scope.devices = response.data;
+        $scope.currentDevice = currentDevice(response.data[0].id);
+        fetchData(response.data[0].id);
+      });
+    };
     console.log($scope);
 
     $scope.updateDevice = function(id) {
@@ -37,8 +41,20 @@ angular.module('particleApp')
         console.log($scope.currentDeviceInfo);
         getVariableVal(id);
 
-        $scope.functionNames = angular.forEach(response.data.functions, function(name){return name;});
-        getFunctions($scope.functionNames, id);
+        $scope.functionNames = angular.forEach(response.data.functions, function(name) {
+          return name;
+        });
+        // getFunctions($scope.functionNames, id);
+
+        $scope.device.lastHeard = {
+          date: new Moment(response.data.lastHeard).format('MMMM, D YYYY'),
+          time: new Moment(response.data.lastHeard).format('h:mm a')
+        };
+        $scope.device.status = response.data.status;
+        // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        $scope.device.lastIpAddress = response.data.last_ip_address;
+        $scope.device.funcNames = response.data.functions;
+
       });
     }
 
@@ -51,23 +67,41 @@ angular.module('particleApp')
         console.log('$scope.variableValInfo');
         console.log($scope.variableValInfo);
         //   angular.forEach($scope.devices, function())
+        $scope.device.varName = response.data.name;
+        $scope.device.varResult = response.data.result;
       });
     }
 
-    function getFunctions(funcs, id) {
-        angular.forEach(funcs, function(func) {
-            console.log(func);
-            console.log(id);
-            var variableValUrl = 'https://api.particle.io/v1/devices/' + id + '/' + func;
-            var data = {key: 'arg=random text&access_token='+ at};
-            $http.post(variableValUrl, data).then(function(response) {
-              $scope.functionInfo = response.data;
-              console.log('$scope.functionInfo');
-              console.log($scope.functionInfo);
-            });
-        });
+    $scope.submitFunc = function(func) {
+      if ($scope.text) {
+        var variableValUrl = 'https://api.particle.io/v1/devices/' + $scope.currentDeviceInfo.id + '/' + func + '/\?arg\="' + this.text + '"\&access_token\=' + at;
 
-    }
+        $http.post(variableValUrl).then(function(response) {
+          console.log(response.data.return_value);
+          $scope.currentDeviceInfo.functions[func] = response.data.return_value;
+        });
+        console.log(func);
+
+        $scope.text = '';
+        console.log($scope.currentDeviceInfo.functions);
+        console.log(this.text);
+
+      }
+    };
+
+    // function getFunctions(funcs, id) {
+    //   angular.forEach(funcs, function(func) {
+    //     var variableValUrl = 'https://api.particle.io/v1/devices/' + id + '/' + func + '/\?arg\="randomText"\&access_token\=' + at;
+    //     // var data = {key: 'arg=random text&access_token='+ at};
+    //     $http.post(variableValUrl).then(function(response) {
+    //       $scope.functionInfo = response.data;
+    //       console.log('$scope.functionInfo');
+    //       console.log(func);
+    //       console.log($scope.functionInfo);
+    //     });
+    //   });
+    //
+    // }
 
     function fetchData(id) {
       getDeviceInfo(id);
